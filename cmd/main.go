a3f3e3f3a124aee24a33b4aa8ece442502d533cf
evaki <zaitovaeva2@mail.ru>
2025-11-23 23:42:02 +0300

    feat(main): add viper for server config and implemented all logic

--- cmd/main.go
+++ cmd/main.go
@@ -9,13 +9,31 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
-
+	"github.com/spf13/viper"
 	"github.com/evakaiing/PR-Reviewer-Assignment-Service/internal/database"
 	"github.com/evakaiing/PR-Reviewer-Assignment-Service/internal/handlers"
 	"github.com/evakaiing/PR-Reviewer-Assignment-Service/internal/repository/user"
+	"github.com/evakaiing/PR-Reviewer-Assignment-Service/internal/repository/team"
+	"github.com/evakaiing/PR-Reviewer-Assignment-Service/internal/repository/pr"
 )
 
+// TODO: (task-2) add validator
+// TODO: (task-3) add middleware for logg
+
+func initConfig() error {
+    viper.AddConfigPath("configs")
+    viper.SetConfigName("config")
+    viper.SetConfigType("yml")
+    viper.AutomaticEnv()
+    
+    return viper.ReadInConfig()
+}
+
 func main() {
+	if err := initConfig(); err != nil {
+        log.Fatalf("failed to initialize configs: %v", err.Error())
+    }
+
 	logger := *slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	db, err := database.NewPostgresDB()
@@ -25,22 +43,34 @@ func main() {
 	defer db.Close()
 
 	userRepo := user.NewRepository(db)
-	userHandler := &handlers.UserHandler{
-		Logger:   logger,
-		UserRepo: userRepo,
-	}
+	teamRepo := team.NewRepository(db)
+	prRepo := pr.NewRepository(db)
+	userHandler := handlers.NewUserHandler(logger, userRepo)
+	teamHandler := handlers.NewTeamHandler(logger, teamRepo)
+	prHandler := handlers.NewPullRequestHandler(logger, prRepo)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello!"))
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"status":"ok"}`))
 	})
-	mux.HandleFunc("/users/setIsActive", userHandler.SetIsActive)
-	mux.HandleFunc("/users/getReview", userHandler.GetReview)
+	mux.HandleFunc("POST /users/setIsActive", userHandler.SetIsActive)
+	mux.HandleFunc("GET /users/getReview", userHandler.GetReview)
 
-	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
-	}
+	mux.HandleFunc("POST /team/add", teamHandler.Add)
+	mux.HandleFunc("GET /team/get", teamHandler.Get)
+
+	mux.HandleFunc("POST /pullRequest/create", prHandler.Create)
+	mux.HandleFunc("POST /pullRequest/merge", prHandler.Merge)
+	mux.HandleFunc("POST /pullRequest/reassign", prHandler.Reassign)
+
+    srv := &http.Server{
+        Addr:         ":" + viper.GetString("server.port"),
+        Handler:      mux,
+        ReadTimeout:  viper.GetDuration("server.read_timeout"),
+        WriteTimeout: viper.GetDuration("server.write_timeout"),
+        IdleTimeout:  viper.GetDuration("server.idle_timeout"),
+    }
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
